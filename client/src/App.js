644a9273c5ddc855baad9b4207086f367eab77aa
Zach Durham <ZachDurham@dhcp-v80-000164.wireless.unc.edu>
2017-10-13 16:48:58 -0400

    initializing files

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const App = () =>
+  <div>
+    <h1>Project 3 App</h1>
+  </div>
+
+export default App;
