280fce621f7c4db8216dfbd840796d9fbcd63cc6
Zach Durham <ZachDurham@dhcp-v80-000164.wireless.unc.edu>
2017-10-14 13:42:44 -0400

    commiting new files

--- routes/api/userEvent.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const userEventController = require("../../controllers/userEventController");
-
-router.route("/createEvent")
-  .post(userEventController.createEvent);
-
-module.exports = router;
