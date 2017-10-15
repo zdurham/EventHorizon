1c0ceaee1a5c42c2e84a8183f06b849756ca9ea0
Monique Hampton <moniquejhampton@yahoo.com>
2017-10-15 12:14:03 -0400

    Changed the navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -4,25 +4,24 @@ import './Nav.css';
 
 const Nav = () =>
   <header>
-    <div className="title-bar" data-responsive-toggle="menu" data-hide-for="medium">
+   {/*} <div className="title-bar" data-responsive-toggle="menu" data-hide-for="medium">
       <button className="menu-icon" type="button" data-toggle="menu"></button>
       <div className="title-bar-title">OverReacted</div>
-    </div>
+    </div> */}
 
     <div className="top-bar">
       <div className="top-bar-left">
         <ul className="dropdown menu" data-dropdown-menu>
-          <li className="menu-text">OverReacted</li>
+          <li className="menu-text">RTP Community Calendar</li>
           <li><a href="/">Home</a></li>
           <li><a href="/about">About</a></li>
-          <li><a href="/calendar">Calendar</a></li>
-          <li><a href="/advertise">Advertise</a></li>
+          <li><a href="/events">Events</a></li>
+          <li><a href="/advertise">Affiliates</a></li>
         </ul>
       </div>
       <div className="top-bar-right">
         <ul className="menu">
-          <li><a href="/user">My Account</a></li>
-          <li><a className="button" href="/logout">Logout</a></li>
+          <li><a className="button" href="/login">Log In</a></li>
         </ul>
       </div>
     </div>
--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 const About = () =>
   <div>
-    <h4>Project 3: ... Researching Foundation. I will most likely use npm (webpack, SASS, @ Foudation) as a solution. https://www.npmjs.com/package/sass-loader</h4>
+    <h4></h4>
     <div className="about">
       <div className="row">
         <div className="large-12 columns">
@@ -14,7 +14,7 @@ const About = () =>
     <div className="info">
       <div className="row">
         <div className="large-12 columns">
-          <h4>Friends Don't Let Friends Read Alone...</h4>
+          <h4></h4>
         </div>
       </div>
     </div>
