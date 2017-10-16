50783b45746fa7a8a34cdc702a8a3f1788988460
cbaddeley <corybaddeley@gmail.com>
2017-10-16 17:41:34 -0400

    added login stuff

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,29 +1,89 @@
-import React from "react";
+import React, { Component } from "react";
 import './Nav.css';
+import Modal from 'react-modal';
+import LogInModal from '../LogInModal';
 
-const Nav = () =>
-  <header>
-   {/*} <div className="title-bar" data-responsive-toggle="menu" data-hide-for="medium">
-      <button className="menu-icon" type="button" data-toggle="menu"></button>
-      <div className="title-bar-title">OverReacted</div>
-    </div> */}
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
-    <div className="top-bar">
-      <div className="top-bar-left">
-        <ul className="dropdown menu" data-dropdown-menu>
-          <li className="menu-text">RTP Community Calendar</li>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/events">Events</a></li>
-          <li><a href="/advertise">Advertise</a></li>
-        </ul>
-      </div>
-      <div className="top-bar-right">
-        <ul className="menu">
-          <li><a className="button" dataRevealId="LogInModal" href="#">Log In</a></li>
-        </ul>
-      </div>
-    </div>
-  </header>;
+class Nav extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalIsOpen: false,
+      username: '',
+      password: ''
+    }
+
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  openModal = () => {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal = () => {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal = () => {
+    this.setState({modalIsOpen: false});
+  }
+
+  render() {
+    return (
+      <header>
+
+        <div className="top-bar">
+          <div className="top-bar-left">
+            <ul className="dropdown menu" data-dropdown-menu>
+              <li className="menu-text">RTP Community Calendar</li>
+              <li><a href="/">Home</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/events">Events</a></li>
+              <li><a href="/advertise">Advertise</a></li>
+            </ul>
+          </div>
+          <div className="top-bar-right">
+            <ul className="menu">
+              <li><button onClick={this.openModal}>Log In</button></li>
+            </ul>
+          </div>
+        </div>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+        
+          <LogInModal
+            username={this.state.username}
+            password={this.state.password}
+            handleInputChange={this.handleInputChange}
+          />
+
+        </Modal>
+      </header>
+    )
+  }
+
+}
 export default Nav;
