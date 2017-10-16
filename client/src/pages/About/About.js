502c36cf33a94c670fdbbfeb945d167758dc4b2f
Tim Acker <tjacker31@gmail.com>
2017-10-16 13:52:53 -0400

    Footer & Custom CSS
    Redid code that was lost from last commit.

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,8 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 
 const About = () =>
   <div>
-    <h4></h4>
     <div className="about">
       <div className="row">
         <div className="large-12 columns">
@@ -14,7 +13,7 @@ const About = () =>
     <div className="info">
       <div className="row">
         <div className="large-12 columns">
-          <h4></h4>
+          <h4>Event</h4>
         </div>
       </div>
     </div>
