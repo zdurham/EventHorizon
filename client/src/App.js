1373fa3af047fe783e150fb444b94533cb7d61eb
Mo Hampton <monique.j.hampton@gmail.com>
2017-10-19 21:28:03 -0400

    Event Component & Event Page Route

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import About from "./pages/About";
 import Events from "./pages/Events";
+import Event from "./components/Event";
 
 const App = () =>
   <Router>
@@ -13,6 +14,7 @@ const App = () =>
       <Nav />
       <Switch>
         <Route exact path="/" component={About}/>
+        <Route exact path="/events" component={Events}/>
       </Switch>
       {/* <Footer /> */}
     </div>
