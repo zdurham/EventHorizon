af61f2a4d2304b476fa11fbb44ce68d07c9473eb
cbaddeley <corybaddeley@gmail.com>
2017-10-22 13:40:34 -0400

    minor filters

--- models/Event.js
+++ models/Event.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const EventSchema = new Schema({
   createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
   name: { type: String },
+  advert: { type: Boolean },
   genre: { type: String },
   description: { type: String },
   date: { type: Date },
