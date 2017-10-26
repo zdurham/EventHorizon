7c0cf2561acb5c6fa874af6dec218efb77bb5613
Mo Hampton <monique.j.hampton@gmail.com>
2017-10-25 22:36:52 -0400

    Update to the event card template

--- client/src/components/DateCard/DateCard.js
+++ client/src/components/DateCard/DateCard.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import FontAwesome from 'react-fontawesome';
 import './DateCard.css';
-import { Row, Col, Card, CardLink, CardText, Collapse } from 'reactstrap';
+import { Row, Col, Card, CardLink, CardTitle, CardText, Collapse } from 'reactstrap';
 
 class DateCard extends Component {
   constructor(props) {
@@ -26,22 +26,17 @@ class DateCard extends Component {
       </Card>
       </Collapse>*/}
 
-      <Card>
-        <CardText className="eventName text-left" onClick={this.toggle} >Event Title</CardText>
-        <CardText>
-            <Col>
-            <Row tag="p">
-        <div className="userName" >Added by:.....   |</div>
-        <div className="eventDate">Date:       |</div>
-        <CardLink href="#"> Event Type </CardLink>
-            </Row>
-            </Col>
+      <Card body className="eventCard">
+        <CardTitle className="eventName text-left" onClick={this.toggle}>Event Name</CardTitle>
+        <CardText >
+        <div className="userName" >Added by:</div>
+        <div className="eventDate">Date: </div>
         </CardText>
         <CardText>
         <Collapse isOpen={this.state.collapse}>
           <CardText>Event description...fun fun fun blah blah blah and all the good stuf that makes you come out of the house.</CardText>
-            <FontAwesome icon="ion-chevron-up" fontSize="15px" color="grey"></FontAwesome>
-            <FontAwesome icon="ion-chevron-down" fontSize="15px" color="grey"></FontAwesome>
+            <FontAwesome className="up-vote" name="thumbs-o-up" size="2x" color="gray"></FontAwesome>
+            <FontAwesome className="down-vote" name="thumbs-o-down" size="2x" color="gray"></FontAwesome>
            </Collapse>
         </CardText>
         {/*<CardFooter>
