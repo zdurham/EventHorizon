b7853f765ed30c2e891ec75ebb8f127202b70ea4
cbaddeley <corybaddeley@gmail.com>
2017-10-26 21:41:14 -0400

    added api call to get attending events

--- models/user.js
+++ models/user.js
@@ -45,8 +45,44 @@ const UserSchema = new Schema({
   createdEvents: [{
     type: Schema.Types.ObjectId,
     ref: 'Event'
+  }],
+  attendingEvents: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Event'
   }]
 })
+
+//Methods for adding events that user is attending
+
+UserSchema.methods.attending = function attending(eventId, cb) {
+  if (this.attendingEvents.indexOf(eventId) >= 0) {
+    this.unattend(eventId, cb);
+  } else {
+    this.attend(eventId, cb);
+  }
+};
+
+UserSchema.methods.unattend = function unattend(eventId, fn) {
+  this.attendingEvents.pull(eventId);
+
+  // If callback fn, save and return
+  if (2 === arguments.length) {
+    this.save(fn);
+  };
+};
+
+UserSchema.methods.attend = function attend(eventId, fn) {
+
+
+  // Attend
+  this.attendingEvents.addToSet(eventId);
+
+  // If callback fn, save and return
+  if (2 === arguments.length) {
+    this.save(fn);
+  };
+};
+
 
 // checking if password is valid
 UserSchema.method('validPassword', function(password) {
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,4 +5,7 @@ const reqLogin = require('../../middleware/reqLogin')
 router.route("/:id")
   .get(reqLogin, userController.getUserEvents);
 
+router.route("/attending/:id")
+  .get(userController.getUserAttendingEvents);
+
 module.exports = router;
