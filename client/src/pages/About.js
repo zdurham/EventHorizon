04079702eaa669ef602df2e39418357db6ba25be
Tim Acker <tjacker31@gmail.com>
2017-10-30 17:35:11 -0400

    Dashboard Button
    Replaced Join Community button with View My Dashboard button on the home page when a user is logged in.

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { Button, Container } from 'reactstrap';
 import Carousel from "../components/Carousel";
 import Info from "../components/Info";
 import JoinBtn from "../components/JoinBtn";
+import { withRouter } from 'react-router';
 
 class About extends Component {
   constructor(props) {
     super(props);
+  }
 
+  handleOnClick = () => {
+    window.scrollTo(0, 0)
+    return this.props.history.push('/dashboard');
   }
 
   render() {
@@ -19,7 +25,14 @@ class About extends Component {
         {!this.props.user ? (
           <JoinBtn />
         ) : (
-          ''
+          <Container className="text-center">
+            <Button
+              size="lg"
+              className="button-primary"
+              onClick={this.handleOnClick}>
+              View My Dashboard
+            </Button>
+          </Container>
         )}
       </main>
     );
@@ -30,4 +43,6 @@ const mapStateToProps = state => ({
   user: state.authUser ? state.authUser.user : undefined
 });
 
-export default connect(mapStateToProps)(About);
+// export default connect(mapStateToProps)(About);
+
+export default withRouter(connect(mapStateToProps)(About));
